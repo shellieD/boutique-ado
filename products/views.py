1e9b9c81480df9e8bb587e3baba8f31a5a7a0ba6
shellieD <shellie.downie13@gmail.com>
2022-06-22 08:50:43 +0000

    Added sorting and product counts to product page

--- products/views.py
+++ products/views.py
@@ -29,7 +29,6 @@ def all_products(request):
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
 
-
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -45,8 +44,8 @@ def all_products(request):
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-    
-    current_sorting = f'(sort)_(direction)'
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
